b370ecc639998384e5bb1b41915b5b2dd2dc4d1a
praveena9515212513 <95266828+praveena9515212513@users.noreply.github.com>
2022-06-16 14:34:15 +0530

    Add files via upload

---
+++ competative coding lab1_code2.py
@@ -0,0 +1,10 @@
+print('enter temperature in celsius:')
+tempC=int(input())
+print('enter temperature in Fahrenheit:')
+tempF=int(input())
+
+tempCtoF = tempC*(9/5)+32
+tempFtoC = (tempF-32)*(5/9)
+
+print(tempCtoF,tempFtoC)
+ 
