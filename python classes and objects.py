08a0b938ce7078dae6705e419503416eb25649c4
praveena9515212513 <95266828+praveena9515212513@users.noreply.github.com>
2022-06-23 13:13:31 +0530

    Update python classes and objects.py

--- python classes and objects.py
+++ python classes and objects.py
@@ -28,7 +28,7 @@ class MyClass3:
 
 obj3 = MyClass3()
 obj3.prin()
-''' there must be self parameter in any object and in init method which is a
+''' there must be self parameter in any method and also in init method which is a
 reference to the current instance of the class, and is used to access variables
 that belongs to the class.'''
 '''It does not have to be named self , you can call it whatever you like, but it
